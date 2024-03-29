6d7aafab7784ba6b1406880ea2bf38bd7e7b0228
HiHighs <degroote.jonas@hotmail.com>
2024-03-29 11:25:17 +0100

    StarRating componenet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+
+import StarRating from './StarRating';
+
+function Test() {
+  const [rating, setRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='red' onSetRating={setRating} />
+      <p>This movie was rated {rating} stars.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/*
     <App />
+  */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
+    <Test />
   </React.StrictMode>
 );
