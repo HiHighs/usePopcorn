3ef36c5940ccc5327a3b4441ac3bcdafa5788f8d
HiHighs <degroote.jonas@hotmail.com>
2024-05-16 18:46:58 +0200

    Search input focus

--- src/Components/Header/Search.js
+++ src/Components/Header/Search.js
@@ -1,4 +1,21 @@
+import { useEffect, useRef } from 'react';
+
 export default function Search({ query, setQuery }) {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputRef.current) return;
+
+      if (e.key === 'Enter') {
+        inputRef.current.focus();
+        setQuery('');
+      }
+    }
+
+    document.addEventListener('keydown', callback);
+  }, [setQuery]);
+
   return (
     <input
       className='search'
@@ -8,6 +25,7 @@ export default function Search({ query, setQuery }) {
       onChange={(e) => {
         setQuery(e.target.value);
       }}
+      ref={inputRef}
     />
   );
 }
